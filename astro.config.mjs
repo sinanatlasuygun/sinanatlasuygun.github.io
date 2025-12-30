82b8e1fc1c8fcbb319fbff03b692b94bd144172a
Sinan Uyğun <sinan_uygun@windowslive.com>
2025-12-31 00:09:39 +0300

    FIX Astro Settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://sinanatlasuygun.github.io',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
